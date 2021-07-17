5690919127351b9a368d12332f67e0cf5bf61b7e
Bang Shin Chul <shinchul.bang@kurlycorp.com>
2021-07-17 10:40:52 +0900

    val 예제를 추가하라

--- src/main/kotlin/variable.kt
+++ src/main/kotlin/variable.kt
@@ -28,7 +28,7 @@ fun variableExample() {
 
     /**
      * 변수 타입과 알맞지 않은 값을 할당하면
-     * 컴파일러에서 Type Mismatch 에러를 리턴합니다.
+     * 컴파일러에서 Type Mismatch 에러를 출력합니다.
      *
      * kotlin은 정적 타입 시스템(static type system)을 사용합니다.
      * 즉, 소스 코드에 정의된 타입을 컴파일러가 알고 있어서
@@ -82,4 +82,20 @@ fun variableExample() {
      * */
     var pi = 3.14
     println(pi)
+
+
+    /**
+     * 때로는 값을 변경할 수 없는 변수를 사용해야 할 경우가 있습니다. (상수)
+     * 이 때에는 val 키워드를 사용하여 변수를 정의합니다.
+     * */
+    val playerName: String = "토마스"
+    println(playerName)
+
+    /**
+     * 만약, val 으로 선언된 변수의 값을 변경한다면
+     * Error:(3, 5) Kotlin: Val cannot be reassigned
+     * 와 같은 에러를 출력합니다.
+     * */
+//    playerName = "마드리갈"
+
 }
