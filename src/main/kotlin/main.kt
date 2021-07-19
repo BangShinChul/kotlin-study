c88b72c409431614bef160485c8107c76d997bcf
방신철 <bsc0227@naver.com>
2021-07-20 00:42:25 +0900

    익명함수 예제를 추가하라 (#10)
    익명함수 예제를 추가하라

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
-    variableExample()
-    controlFlowAction()
-    functionExamples()
+//    variableExample()
+//    controlFlowAction()
+//    functionExamples()
+    anonymousFunctionExamples()
 }
